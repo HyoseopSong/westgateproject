a13cf61883543a7dac4cc690773e16847fc3a4a7
HyoseopSong <csnvshs@naver.com>
2017-09-04 11:25:47 +0900

    ShopMapInfo 전달하는 Controller 구현

---
+++ westgateprojectService/Controllers/GetShopMapInfoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using westgateprojectService.DataObjects;
+
+namespace westgateprojectService.Controllers
+{
+    [MobileAppController]
+    public class GetShopMapInfoController : ApiController
+    {
+        public List<ShopMapInfoEntity> Get(string buildingFloor)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("ShopMapInfo");
+
+            TableQuery<ShopMapInfoEntity> queryID = new TableQuery<ShopMapInfoEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, buildingFloor));
+
+            List<ShopMapInfoEntity> result = new List<ShopMapInfoEntity>();
+            foreach (ShopMapInfoEntity entity in table.ExecuteQuery(queryID))
+            {
+                result.Add(entity);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ westgateprojectService/DataObjects/ShopMapInfoEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace westgateprojectService.DataObjects
+{
+    public class ShopMapInfoEntity : TableEntity
+    {
+        public ShopMapInfoEntity()
+        {
+
+        }
+
+        public ShopMapInfoEntity(string BuildingFloor, string ShopName)
+        {
+            PartitionKey = BuildingFloor;
+            RowKey = ShopName; // public string Text { get; set; }
+        }
+
+
+        public string BackgroundColor { get; set; }
+        public string TextColor { get; set; }
+        public int FontSize { get; set; }
+        public double XPosition { get; set; }
+        public double YPosition { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+}
